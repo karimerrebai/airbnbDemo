671e3aeff12289ec65b6622622cbf001fdba1a8f
Amine Khmissi <khmissiamine111@gmail.com>
2022-11-11 09:23:50 +0100

    adding commentaire,customer,owner,reservation models

---
+++ Models/Owner.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose'); // Erase if already required
+const User=require('./user')
+// Declare the Schema of the Mongo model
+var ownerSchema = new mongoose.Schema({
+    picture:{
+        type:String,
+        required:false,
+    },
+    matricule:{
+        type:String,
+        required:false,
+        unique:true,
+    },
+    //relation
+    loges:[{
+        type:mongoose.Types.ObjectId,
+        ref:'Loge',
+        required:false
+    }]
+},{timestamps:true});
+
+//Export the model
+module.exports = User.discriminator('Owner', ownerSchema);
---
+++ Models/Reservation.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var reservationSchema = new mongoose.Schema({
+    nights:{
+        type:Number,
+        required:true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    checkIn:{
+        type:Date,
+        required:true,
+    },
+    checkOut:{
+        type:Date,
+        required:true,
+    },
+    //relation:
+    customer:{
+     type:mongoose.Types.ObjectId,
+     ref:'Customer',
+     required:true
+    },
+    facture:{
+     type:mongoose.Types.ObjectId,
+     ref:'Facture',
+     required:false
+    },
+    loge:{
+        type:mongoose.Types.ObjectId,
+        ref:'Loge',
+        required:true
+    }
+},{timestamps:true});
+
+//Export the model
+module.exports = mongoose.model('Reservation', reservationSchema);
